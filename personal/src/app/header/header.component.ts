d66109e4523209bd0520b98cf2ad3b5db72935e3
Deiziane <deiziane_ns@hotmail.com>
2019-08-24 18:51:06 -0300

    add styles to nav

--- personal/src/app/header/header.component.ts
+++ personal/src/app/header/header.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+    const iconMenu = document.querySelector('.menu-burguer');
+
+    iconMenu.addEventListener('click', () => {
+      if (iconMenu.classList.contains('open')) {
+        iconMenu.classList.remove('open');
+      } else {
+        iconMenu.classList.add('open');
+      }
+    }, false);
   }
 
 }
