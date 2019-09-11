86874ab4eb3807da542bdef5de9a862f6826001b
Deiziane <deiziane_ns@hotmail.com>
2019-09-11 19:43:20 -0300

    add html

--- personal/src/app/app-routing.module.ts
+++ personal/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'project', component: ProjectComponent },
 ];
 
 @NgModule({
