151800b2539eb30cc7deabc3ef4c25b83b78defd
Deiziane <deiziane_ns@hotmail.com>
2019-09-16 21:00:36 -0300

    add menu

--- personal/src/app/header/header.component.ts
+++ personal/src/app/header/header.component.ts
@@ -9,15 +9,6 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const iconMenu = document.querySelector('.menu-burguer');
-
-    iconMenu.addEventListener('click', () => {
-      if (iconMenu.classList.contains('open')) {
-        iconMenu.classList.remove('open');
-      } else {
-        iconMenu.classList.add('open');
-      }
-    }, false);
   }
 
 }
