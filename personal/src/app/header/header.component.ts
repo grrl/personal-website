7fc1835461ed356505811c22f1727dec42b68c74
Deiziane <deiziane_ns@hotmail.com>
2019-09-18 16:02:29 -0300

    add menu animation

--- personal/src/app/header/header.component.ts
+++ personal/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TimelineMax, TweenMax } from 'gsap'
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  // hamburger = document.querySelector('.menu');
+  // hamburgerLines = document.querySelectorAll('.menu line');
+  // navOpen = document.querySelector('.burger-open');
+  // textLeft = document.querySelectorAll('.link-left a');
+
+  menu = new TimelineMax({paused: true, reversed: true});
+
   constructor() { }
 
   ngOnInit() {
+    this.init();
   }
+
+  init() {
+    this.menu.to('.burger-open', 0.5, {y: 0}, 0);
+  }
+
+  menuClick() {
+    this.menu.reversed() ? this.menu.play() : this.menu.reverse();	
+    return console.log('clicked');
+   }
 
 }
